3de1aaa4225fd2b65a860d374ac3c2385bc01cae
Sana Akram <you@example.com>
2022-11-01 11:33:22 +0500

    Project Form Template

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
-    path('users/', include('users.urls')),
+    path('projects', include('projects.urls')),
+    path('', include('users.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
